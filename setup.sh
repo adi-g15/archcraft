e96cbd7a9a3bb7aaae39e109f758d6df13942106
adi-g15 <ag15035@gmail.com>
2021-09-24 13:37:44 +0530

    Removals, chaotic-aur 'should' be available

--- setup.sh
+++ setup.sh
@@ -65,6 +65,11 @@ prerequisite() {
 	cp -f /usr/bin/mkarchiso "$DIR"/iso/mkarchcraftiso && chmod +x "$DIR"/iso/mkarchcraftiso
 	sed -i -e 's/-c -G -M/-i -c -G -M/g' "$DIR"/iso/mkarchcraftiso
 
+	# set up chaotic-aur (https://aur.chaotic.cx/)
+	pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+	pacman-key --lsign-key 3056513887B78AEB
+	pacman -U --root "$DIR"/iso/airootfs/ 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
 	# do not delete package database
 	sed -i -e 's|\[\[ -d \"${pacstrap_dir}/var/lib/pacman/sync\"|#\[\[ -d \"${pacstrap_dir}/var/lib/pacman/sync\"|g' "$DIR"/iso/mkarchcraftiso
 
